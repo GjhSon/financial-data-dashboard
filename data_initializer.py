0e034dd72fa88dc7b8e0b469c832befe351f0544
GjhSon <thswjdgus456@gmail.com>
2026-01-30 17:33:23 +0900

    Update data_initializer.py

--- data_initializer.py
+++ data_initializer.py
@@ -4,7 +4,7 @@ import urllib.parse
 
 # 연결엔진
 user = "root"
-password = "qwezxc123456@"
+password = ""
 safe_password = urllib.parse.quote_plus(password)
 host = "localhost"
 port = "3306"
@@ -20,3 +20,4 @@ df = df.rename(columns = {'day' : '_day'})
 df['ta_ymd'] = pd.to_datetime(df['ta_ymd'], format = '%Y%m%d')
 df.to_sql(name = 'yongin_card_usage_11', con = engine, if_exists = 'append', index = False)
 print("Success!")
+
